ad501c23edcec04b259399fe58f4b6385e1921a5
tobias <tobias.omondi@student.moringaschool.com>
2023-10-31 18:00:24 +0300

    updated

--- charity/src/App.js
+++ charity/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+import Project from './components/Project';
+import Blog from './components/Blog';
+import Donate from './components/Donate';
+import Homepage from './components/Homepage';
+import Team from './components/Team';
+import Login from './components/Login';
+import Signin from './components/Signin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path = 'Project' element = {<Project/>} />
+          <Route path = 'Team' element = {<Team/>} />
+          <Route path='Blog' element={<Blog/>}/>
+          <Route path='login' element = {<Login/>}/>
+          <Route path='Signin' element = {<Signin/>}/>
+          <Route path = 'Donate' element = {<Donate/>}/>
+        </Routes>
+      </Router>
+    
     </div>
   );
 }
---
+++ charity/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='navbar-container'>
+        <Link to = '/' className='navabar-logo'>
+
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
